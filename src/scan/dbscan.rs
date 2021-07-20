21fd0c8f560b6bb658bb50648277d6cf10624736
liushuyu <liushuyu011@gmail.com>
2021-07-20 17:46:33 -0600

    scan/dbscan: use a CTE to handle duplicated package insertion

--- src/scan/dbscan.rs
+++ src/scan/dbscan.rs
@@ -280,33 +280,18 @@ RETURNING (xmax = 0) AS new"#,
     ).fetch_one(&mut *pool).await?;
     if !result.new.unwrap_or(false) {
         warn!("{} is a duplicate!", package.filename);
-        // remove duplicated data
+        // remove duplicated data and append this package to duplicate list
         sqlx::query!(
-            "DELETE FROM pv_package_sodep WHERE package=$1 AND version=$2 AND repo=$3",
+            r#"with d1 as (DELETE FROM pv_package_sodep WHERE package=$1 AND version=$2 AND repo=$3 returning package)
+, d2 as (DELETE FROM pv_package_files WHERE package=$1 AND version=$2 AND repo=$3 returning package)
+, d3 as (DELETE FROM pv_package_dependencies WHERE package=$1 AND version=$2 AND repo=$3 returning package)
+, d4 as (DELETE FROM pv_package_duplicate where package=$1 AND version=$2 AND repo=$3 returning package)
+INSERT INTO pv_package_duplicate SELECT * FROM pv_packages WHERE filename=$4"#,
             meta.name,
             meta.version,
-            repo
-        )
-        .execute(&mut *pool)
-        .await?;
-        sqlx::query!(
-            "DELETE FROM pv_package_files WHERE package=$1 AND version=$2 AND repo=$3",
-            meta.name,
-            meta.version,
-            repo
-        )
-        .execute(&mut *pool)
-        .await?;
-        sqlx::query!(
-            "DELETE FROM pv_package_dependencies WHERE package=$1 AND version=$2 AND repo=$3",
-            meta.name,
-            meta.version,
-            repo
-        )
-        .execute(&mut *pool)
-        .await?;
-        // append this package to duplicate list
-        // TODO:
+            repo,
+            package.filename
+        ).execute(&mut *pool).await?;
     }
     // update dependencies information
     for dep in PKG_RELATION {
