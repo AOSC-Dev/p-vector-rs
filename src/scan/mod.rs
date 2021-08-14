724b9c04565cc479dbcfcc47c0a8bfbed8107dcf
liushuyu <liushuyu011@gmail.com>
2021-08-13 21:48:40 -0600

    scan: drop the simple versions of the scanning procedure ...
    ... the plan is scrapped for now. P-Vector will focus on the
    database-based scanning for now.

--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -6,14 +6,9 @@ use log::{error, info};
 use rayon::prelude::*;
 use sha2::{Digest, Sha256};
 use std::fs::Metadata;
-use std::io::SeekFrom;
 use std::path::PathBuf;
 use std::time::UNIX_EPOCH;
-use std::{
-    fs::File,
-    io::{Read, Seek},
-    path::Path,
-};
+use std::{io::Read, path::Path};
 use tar::Archive as TarArchive;
 use walkdir::{DirEntry, WalkDir};
 use xz2::read::XzDecoder;
@@ -72,46 +67,6 @@ fn determine_format(format: &[u8]) -> Result<TarFormat> {
     }
 }
 
-/// Simple deb package reader. Only reads control files. Contents are ignored.
-fn open_deb_simple<R: Read>(reader: R) -> Result<Vec<u8>> {
-    let mut deb = ArArchive::new(reader);
-    while let Some(entry) = deb.next_entry() {
-        if entry.is_err() {
-            continue;
-        }
-        let entry = entry?;
-        let filename = entry.header().identifier();
-        if filename.starts_with(b"control.tar") {
-            let format = determine_format(filename)?;
-            let control = open_compressed_control(entry, &format)?;
-            return Ok(control);
-        }
-    }
-
-    Err(anyhow!("data archive not found or format unsupported"))
-}
-
-/// Simple version of scanning deb packages. No bell and whistles, just copying control files.
-fn scan_single_deb_simple<P: AsRef<Path>>(path: P, root: P) -> Result<Vec<u8>> {
-    let mut f = File::open(path.as_ref())?;
-    let sha256 = sha256sum(&mut f)?;
-    let actual_size = f.seek(SeekFrom::Current(0))?;
-    f.seek(SeekFrom::Start(0))?;
-    let mut control = open_deb_simple(f)?;
-    control.reserve(128);
-    if control.ends_with(&b"\n\n"[..]) {
-        control.pop();
-    }
-    let rel_path = path.as_ref().strip_prefix(root)?;
-    control.extend(format!("Size: {}\n", actual_size).as_bytes());
-    control.extend(format!("Filename: {}\n", rel_path.to_string_lossy()).as_bytes());
-    control.extend(b"SHA256: ");
-    control.extend(sha256.as_bytes());
-    control.extend(b"\n\n");
-
-    Ok(control)
-}
-
 /// Calculate the Sha256 checksum of the given stream
 pub fn sha256sum<R: Read>(mut reader: R) -> Result<String> {
     let mut hasher = Sha256::new();
@@ -127,25 +82,6 @@ fn is_deb(entry: &DirEntry) -> bool {
         .to_str()
         .map(|s| s.ends_with(".deb"))
         .unwrap_or(false)
-}
-
-/// Simple version of scanning, for use in local repository or personal repository
-pub fn scan_packages_simple(entries: &[DirEntry], root: &Path) -> Vec<u8> {
-    entries
-        .par_iter()
-        .map(|entry| -> Vec<u8> {
-            let path = entry.path();
-            info!("{:?}", path);
-            match scan_single_deb_simple(path, root) {
-                Ok(entry) => entry,
-                Err(err) => {
-                    error!("{:?}", err);
-                    Vec::new()
-                }
-            }
-        })
-        .flatten()
-        .collect()
 }
 
 pub fn scan_packages_advanced(entries: &[&Path], root: &Path) -> Vec<PackageMeta> {
@@ -194,11 +130,4 @@ pub fn collect_all_packages<P: AsRef<Path>>(path: P) -> Result<Vec<DirEntry>> {
     }
 
     Ok(files)
-}
-
-#[test]
-fn test_deb() {
-    let content =
-        scan_single_deb_simple("tests/fixtures/a2jmidid_9-0_amd64.deb", "tests/fixtures").unwrap();
-    println!("{}", String::from_utf8(content).unwrap());
 }
