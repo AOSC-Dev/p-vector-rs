05b5c3df7f4b0d57bb12662ad690bf6d3a6c1c15
liushuyu <liushuyu011@gmail.com>
2025-05-21 16:31:14 +0800

    migrations/20210621205250_vercomp.up.sql: fix compatiblity with pg 17

--- migrations/20210621205250_vercomp.up.sql
+++ migrations/20210621205250_vercomp.up.sql
@@ -17,19 +17,19 @@ RETURNS text AS $$
   SELECT string_agg(translate(v[1] || '|',
     '~|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+-.',
     '0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefgh') ||
-    (CASE WHEN v[2]='' THEN '' ELSE _comparable_digit(v[2]) END), '')
+    (CASE WHEN v[2]='' THEN '' ELSE public._comparable_digit(v[2]) END), '')
   FROM q1
 $$ LANGUAGE SQL IMMUTABLE STRICT PARALLEL SAFE COST 200;
 
 CREATE OR REPLACE FUNCTION public.comparable_dpkgver (ver text)
 RETURNS text AS $$
   SELECT ecmp || '!' || (CASE WHEN array_length(spl, 1)=1
-    THEN comparable_ver(spl[1]) || '!1'
-    ELSE comparable_ver(array_to_string(spl[1:array_length(spl, 1)-1], '-'))
-    || '!' || comparable_ver(spl[array_length(spl, 1)]) END)
+    THEN public.comparable_ver(spl[1]) || '!1'
+    ELSE public.comparable_ver(array_to_string(spl[1:array_length(spl, 1)-1], '-'))
+    || '!' || public.comparable_ver(spl[array_length(spl, 1)]) END)
   FROM (
     SELECT (CASE WHEN epos=0 THEN '00'
-      ELSE _comparable_digit(substr(v, 1, epos-1)) END) ecmp, string_to_array(
+      ELSE public._comparable_digit(substr(v, 1, epos-1)) END) ecmp, string_to_array(
       CASE WHEN epos=0 THEN v ELSE substr(v, epos+1) END, '-') spl
     FROM (SELECT position(':' in ver) epos, ver v) q1
   ) q1
